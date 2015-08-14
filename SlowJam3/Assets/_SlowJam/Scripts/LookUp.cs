4c41a64983f732de77f95ea9a71a1c67480015c9
Brandon Farrell <chees502@gmail.com>
2015-08-14 09:32:36 -0700

    lookup systems to provide more details (cosmetic) information about the player.

---
+++ SlowJam3/Assets/_SlowJam/Scripts/LookUp.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LookUp : MonoBehaviour {
+
+	private static int[]     _positions      = {1,           2,              0,          3};
+    private static Color[]   _colors         = {Color.red,   Color.yellow,   Color.blue, Color.green};
+    private static string[]  _colorNames     = { "Red",      "Yellow",       "Blue",     "Green" };
+
+    /// <summary>
+    /// Gets the players postion in the cabnet starting at 0 on the left.
+    /// </summary>
+    /// <param name="playerNumber"></param>
+    /// <returns></returns>
+    public static int PlayerPosition(int playerNumber)
+    {
+        return _positions[playerNumber];
+    }
+    /// <summary>
+    /// Gets the Color assigned to that player
+    /// </summary>
+    /// <param name="playerNumber"></param>
+    /// <returns></returns>
+    public static Color PlayerColor(int playerNumber)
+    {
+        return _colors[playerNumber];
+    }
+    /// <summary>
+    /// Gets the string of the Color assigned to that player.
+    /// </summary>
+    /// <param name="playerNumber"></param>
+    /// <returns></returns>
+    public static string PlayerColorName(int playerNumber)
+    {
+        return _colorNames[playerNumber];
+    }
+}
