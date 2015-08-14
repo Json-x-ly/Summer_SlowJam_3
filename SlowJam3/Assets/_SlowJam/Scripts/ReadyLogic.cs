755ff9686b3342cc98e3979e35e223383736d804
Brandon Farrell <chees502@gmail.com>
2015-08-14 11:12:10 -0700

    Global game state manager, and ready up screen.

---
+++ SlowJam3/Assets/_SlowJam/Scripts/ReadyLogic.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ReadyLogic : MonoBehaviour {
+    bool isReady = false;
+    const float readyPos = 10;
+    const float idlePos = 5;
+    Vector3 targetPos;
+    public bool toggle;
+    public int player=-1;
+	// Use this for initialization
+	void Awake () {
+        targetPos = transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (toggle)
+        {
+            toggle = false;
+            ToggleReady();
+        }
+	    if(TinderBox.TinderBoxAPI.ControlDown(player,TinderBox.Controls.Button5)){
+            ToggleReady();
+        }
+        transform.position=Vector3.Lerp(transform.position, targetPos, Time.deltaTime*10);
+        
+	}
+    public void ToggleReady()
+    {
+        isReady = !isReady;
+        if (isReady)
+        {
+            targetPos.y = readyPos;
+        }
+        else
+        {
+            targetPos.y = idlePos;
+        }
+    }
+    public void SetPlayerNumber(int player){
+        this.player = player;
+        MeshRenderer mr = gameObject.GetComponent<MeshRenderer>();
+        mr.material = Resources.Load("Solid"+ LookUp.PlayerColorName(player))as Material;
+    }
+    public void RemoveReadyCard()
+    {
+        Debug.Log("removed ready card");
+        Destroy(gameObject);
+    }
+}
---
+++ SlowJam3/Assets/_SlowJam/Scripts/_root.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class _root : MonoBehaviour {
+    public enum _state { SplashScreen, Ready, Player, Lose };
+    private static _state __state = _state.SplashScreen;
+    public static _state state
+    {
+        get
+        {
+            return __state;
+        }
+        set
+        {
+            if (value == __state) return;
+            StateChange(__state, value);
+            __state = value;
+        }
+
+    }
+    private static void StateChange(_state old, _state now)
+    {
+        Debug.Log("State has changed from " + old.ToString() + " to " + now.ToString());
+        switch (now)
+        {
+            case(_state.Ready):
+                SpawnReadyCards();
+                break;
+        }
+        switch (old)
+        {
+            case(_state.Ready):
+                ReadyLogic[] scripts = FindObjectsOfType<ReadyLogic>();
+                foreach (ReadyLogic script in scripts)
+                {
+                    script.RemoveReadyCard();
+                }
+
+                break;
+        }
+    }
+	// Use this for initialization
+	void Awake () {
+        //TinderBox.TinderBoxAPI.IsReady();
+        state = _state.Ready;
+	}
+    static readonly Vector3 rcStartPos = new Vector3(-5, 5, 1);
+    static readonly Vector3 rcSize = new Vector3(3, 5, 1);
+    const float rcStepLength = 3+1.0f / 3.0f;
+    public static void SpawnReadyCards()
+    {
+        for (int x = 0; x < 4; x++)
+        {
+            int cabPos = LookUp.PlayerPosition(x);
+            GameObject go = GameObject.CreatePrimitive(PrimitiveType.Quad);
+            go.name = "ReadyPlayer " + x;
+            go.transform.position = rcStartPos + Vector3.right * cabPos * rcStepLength;
+            go.transform.localScale = rcSize;
+            ReadyLogic script = go.AddComponent<ReadyLogic>();
+            script.SetPlayerNumber(x);
+        }
+    }
+}
