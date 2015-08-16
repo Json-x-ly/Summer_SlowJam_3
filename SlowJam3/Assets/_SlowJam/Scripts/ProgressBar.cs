a78bb07e7ebb39e7f1a8373bd64fcf103802dc83
akb165 <akb165@gmail.com>
2015-08-16 03:42:31 -0700

    Bars
    Progress and stamina bars

---
+++ SlowJam3/Assets/_SlowJam/Scripts/ProgressBar.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class ProgressBar : MonoBehaviour {
+	
+	private long progress = 50;
+	public GameObject progressBar;
+	
+	void Start()
+	{
+		progressBar = Instantiate (progressBar);
+		progressBar.transform.parent = transform;
+	}
+	
+	void Update()
+	{
+		Debug.Log (progressBar.transform.position);
+		Slider progScript = progressBar.GetComponentInChildren<Slider> ();
+		progScript.value = progress;
+	}
+}
